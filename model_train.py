fa6157d0fa2f8a1549108e4c81372377e6f84f07
Wai-Ching Leung <36772713+chingachleung@users.noreply.github.com>
2022-12-08 12:02:28 -0500

    Update model_train.py

--- model_train.py
+++ model_train.py
@@ -8,17 +8,17 @@ device = 'cuda' if cuda.is_available() else 'cpu'
 LEARNING_RATE = 1e-05
 
 class EarlyStopper:
-    def __init__(self, patience=3, min_delta=0):
+    def __init__(self, patience=3, min_delta=-0.001):
         self.patience = patience
         self.min_delta = min_delta
         self.counter = 0
         self.min_validation_loss = np.inf
 
     def early_stop(self, validation_loss):
-        if validation_loss < self.min_validation_loss:
+        if validation_loss < (self.min_validation_loss + self.min_delta):
             self.min_validation_loss = validation_loss
             self.counter = 0
-        elif validation_loss > (self.min_validation_loss + self.min_delta):
+        else:
             self.counter += 1
             if self.counter >= self.patience:
                 return True
